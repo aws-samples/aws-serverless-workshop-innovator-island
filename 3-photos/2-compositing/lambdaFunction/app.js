cff72ec37b4b67a28815206c0af250eca3efd1c8
Fernanda Machado <machafer@amazon.com>
2023-03-30 13:16:00 +0200

    Fixed code style

--- 3-photos/2-compositing/lambdaFunction/app.js
+++ 3-photos/2-compositing/lambdaFunction/app.js
@@ -12,12 +12,12 @@ const fs = require('fs');
 
 // Wrapping promise around Jimp callback
 const getBuffer = function(image) {
-  return new Promise((resolve, reject) => {
-    image.getBuffer(Jimp.MIME_PNG, (err, data) => {
-      if (err) reject(err)
-      resolve(data)
+    return new Promise((resolve, reject) => {
+        image.getBuffer(Jimp.MIME_PNG, (err, data) => {
+            if (err) reject(err)
+            resolve(data)
+        })
     })
-  })
 }
 
 // Module 3 - Compositing
@@ -26,40 +26,40 @@ const getBuffer = function(image) {
 
 exports.handler = async (event) => {
 
-  const params = {
-    Bucket: event.Records[0].s3.bucket.name,
-    Key: event.Records[0].s3.object.key
-  }
+    const params = {
+        Bucket: event.Records[0].s3.bucket.name,
+        Key: event.Records[0].s3.object.key
+    }
 
-  // Load greenscreen person foreground (already resized to 600w x 800h in previously Lambda function)
-  const s3Object = await s3.getObject(params).promise()
-  const foreground  = await Jimp.read(s3Object.Body)
+    // Load greenscreen person foreground (already resized to 600w x 800h in previously Lambda function)
+    const s3Object = await s3.getObject(params).promise()
+    const foreground  = await Jimp.read(s3Object.Body)
 
-  // Select random background (1-4 available)
-  const random = Math.ceil(Math.random()*4)
-  const background = await Jimp.read( `https://d15l97sovqpx31.cloudfront.net/images/composite-bg${random}.png`) // theme park background
-  const branding = await Jimp.read('https://d15l97sovqpx31.cloudfront.net/images/edge-decor-600x1000.png') // branding frame
+    // Select random background (1-4 available)
+    const random = Math.ceil(Math.random()*4)
+    const background = await Jimp.read( `https://d15l97sovqpx31.cloudfront.net/images/composite-bg${random}.png`) // theme park background
+    const branding = await Jimp.read('https://d15l97sovqpx31.cloudfront.net/images/edge-decor-600x1000.png') // branding frame
 
-  // Composite background with greenscreen foreground (foreground in front - added vertical offset of 130px)
-  const x = (background.bitmap.width/2) - (foreground.bitmap.width/2) // updated code here to center photo on background
-  let composited = await background.composite(foreground, x, 130, { mode: Jimp.BLEND_SOURCE_OVER })
+    // Composite background with greenscreen foreground (foreground in front - added vertical offset of 130px)
+    const x = (background.bitmap.width/2) - (foreground.bitmap.width/2) // updated code here to center photo on background
+    let composited = await background.composite(foreground, x, 130, { mode: Jimp.BLEND_SOURCE_OVER })
 
-  // Composite with branding frame (branding in front)
-  composited = await composited.composite(branding, 0, 0, { mode: Jimp.BLEND_SOURCE_OVER })
+    // Composite with branding frame (branding in front)
+    composited = await composited.composite(branding, 0, 0, { mode: Jimp.BLEND_SOURCE_OVER })
 
-  // Save to temp location as JPEG
-  const output_filename = params.Key.replace('.png', '.jpg')
-  const output_path = `/tmp/${output_filename}`
-  await composited.writeAsync(output_path)
+    // Save to temp location as JPEG
+    const output_filename = params.Key.replace('.png', '.jpg')
+    const output_path = `/tmp/${output_filename}`
+    await composited.writeAsync(output_path)
 
-  // Save to S3
-  const outParams = {
-    Bucket: process.env.OUTPUT_BUCKET_NAME,
-    Key: output_filename,
-    ContentType: Jimp.MIME_JPEG,
-    Body: fs.readFileSync(output_path),
-    //ACL: 'public-read'
-  }
-  console.log(outParams)
-  console.log(await s3.putObject(outParams).promise())
+    // Save to S3
+    const outParams = {
+        Bucket: process.env.OUTPUT_BUCKET_NAME,
+        Key: output_filename,
+        ContentType: Jimp.MIME_JPEG,
+        Body: fs.readFileSync(output_path),
+        //ACL: 'public-read'
+    }
+    console.log(outParams)
+    console.log(await s3.putObject(outParams).promise())
 }
